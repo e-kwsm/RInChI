748d71aafc268a736efeb3c849fec29a99e921cf
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2025-04-11 00:44:49 +0900

    src/test/test_suite/tests/USPTO_patent_data_tests.cpp

--- src/test/test_suite/tests/USPTO_patent_data_tests.cpp
+++ src/test/test_suite/tests/USPTO_patent_data_tests.cpp
@@ -59,11 +59,13 @@ void USPTOPatentDataTests::rxnfiles_subset()
 
 	int test_count = 0;
 	for (boost::filesystem::directory_iterator testfile(current_dir); testfile != end_itr; testfile++) {
-		if (!boost::filesystem::is_regular_file(testfile->path()))
+		if (!boost::filesystem::is_regular_file(testfile->path())) {
 			continue;
+		}
 		std::string test_filename = testfile->path().string();
-		if (test_filename.substr(test_filename.length() - 4, 4) != ".rxn")
+		if (test_filename.substr(test_filename.length() - 4, 4) != ".rxn") {
 			continue;
+		}
 
 		test_count++;
 
@@ -77,8 +79,9 @@ void USPTOPatentDataTests::rxnfiles_subset()
 		std::string expected_rinchi_string;
 		std::string expected_rinchi_auxinfo;
 		std::ifstream expected_file ( test_filename_rinchis.c_str() );
-		if (!expected_file)
+		if (!expected_file) {
 			throw rinchi::unit_test::TestFailure (test_filename_rinchis + " does not exist.");
+		}
 		rinchi::rinchi_getline(expected_file, expected_rinchi_string);
 		rinchi::rinchi_getline(expected_file, expected_rinchi_auxinfo);
 
